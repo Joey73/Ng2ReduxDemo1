f94ff4f03f7080dc61197d7f68ec6642f14b4804
Joerg Schmitz <schmitz873@gmail.com>
2017-12-12 00:04:12 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgRedux } from 'ng2-redux'
+import { IAppState } from './store';
+import { INCREMENT } from './actions';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  counter: number = 0;
+
+  constructor(private ngRedux: NgRedux<IAppState>){
+
+  }
+
+  increment(){
+    // this.counter++; // this is how you usually would do it
+    
+    /*
+      Instead you dispatch an action (Action -> Store -> Reducer).
+      The reducer will give back a new state to the store.
+      The store will update the state internally.
+    */
+    this.ngRedux.dispatch({type: INCREMENT});  // pass an action object with the type    
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { NgRedux, NgReduxModule } from 'ng2-redux'
 
 import { AppComponent } from './app.component';
+import { IAppState, rootReducer, INITIAL_STATE } from './store';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgReduxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(ngRedux: NgRedux<IAppState>){
+    ngRedux.configureStore(rootReducer, INITIAL_STATE);
+  }
+}
